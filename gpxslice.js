ec16c79c043f84c1806a32d580bfd06d910b4b71
Steve Gravrock <sdg@panix.com>
2019-10-05 20:11:10 -0700

    Create track slices (as JSON, for the moment)

--- gpxslice.js
+++ gpxslice.js
@@ -2,18 +2,55 @@
 	document.querySelector("#file").addEventListener("change", function(e) {
 		e.target.files[0].text()
 			.then(function(text) {
-				showMap(parseTrack(text));
+				init(parseTrack(text));
 			});
 	});
 
-	function showMap(track) {
+	function init(track) {
 		document.body.className = "showing-map";
-		var map = L.map('map');
-		var line = L.polyline(track, {color: 'red'});
+		const map = showMap(track);
+
+		const start = document.querySelector("[name=start]")
+		start.value = 0;
+		createMarker(start, track, map);
+
+		const end = document.querySelector("[name=end]");
+		end.value = track.length - 1;
+		createMarker(end, track, map);
+
+		document.querySelector("#slice").addEventListener("click", function() {
+			createSlice(track);
+		});
+	}
+
+	function showMap(track) {
+		const map = L.map('map');
+		const line = L.polyline(track, {color: 'red'});
 		line.addTo(map);
 		map.fitBounds(line.getBounds());
-		var layer = new L.StamenTileLayer("terrain");
-		map.addLayer(layer);
+		map.addLayer(new L.StamenTileLayer("terrain"));
+
+		return map;
+	}
+
+	function createMarker(input, track, map) {
+		const marker = L.marker(getLatLng());
+		marker.addTo(map);
+
+		input.addEventListener("change", function() {
+			marker.setLatLng(getLatLng());
+		});
+
+		function getLatLng() {
+			return track[parseInt(input.value, 10)];
+		}
+	}
+
+	function createSlice(track) {
+		const start = parseInt(document.querySelector("[name=start]").value, 10);
+		const end = parseInt(document.querySelector("[name=end]").value, 10);
+		const slice = track.slice(start, end + 1);
+		document.querySelector("#output").textContent = JSON.stringify(slice);
 	}
 
 	function parseTrack(src) {
